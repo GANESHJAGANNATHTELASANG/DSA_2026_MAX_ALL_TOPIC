8df50da5ca2d0d78aa41a3076ae67890069ceb50
GANESHJAGANNATHTELASANG <ganesh.telasang8@gmail.com>
2026-07-16 06:30:16 +0530

    today we done 5 question on array basic dsa questions

---
+++ array's/arra_linear_search.java
@@ -0,0 +1,25 @@
+public class arra_linear_search{
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 40;
+        int index = -1;
+        
+        for(int i=0; i<=arr.length ; i++){
+            if(target == arr[i]){
+                System.out.println("target founded");
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1){
+            System.out.println("the element is not found");
+        }else{
+            System.out.println("the element was founded in " + (index) );
+
+        }
+
+       
+       
+    }
+}
