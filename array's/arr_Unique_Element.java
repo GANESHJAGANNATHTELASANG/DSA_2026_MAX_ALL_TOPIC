428ecae4972172601308195411bb948ea6896959
GANESHJAGANNATHTELASANG <ganesh.telasang8@gmail.com>
2026-07-22 06:12:52 +0530

    it is the question on array whichwe did not solved yestarday so this is yestareday problem thenk you and problem name is finding a unique value in a array

---
+++ array's/arr_Unique_Element.java
@@ -0,0 +1,28 @@
+public class arr_Unique_Element {
+    public static void main(String[] args) {
+        int [] arr = {1,3,4,4,3,1,2};
+        int count = 0;
+        int unique = arr[0];
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0;j<arr.length; j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+              
+
+            }
+
+             if(count==1){
+                unique = arr[i];
+                break;
+            } 
+
+
+        }
+
+                   
+        
+    }
+    
+}
